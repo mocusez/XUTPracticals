964f042285d02c1efa8427625802349f5c8e8517
Mocus EZ <285918468@qq.com>
2021-09-26 21:45:49 +0800

    Update temperatures.py

---
+++ prac_03/temperatures.py
@@ -0,0 +1,43 @@
+"""
+CP1404/CP5632 - Practical
+Pseudocode for temperature conversion
+"""
+
+
+def main():
+    MENU = """C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        change(choice)
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def change(choice):
+    if choice == "C":
+        celsius = float(input("Celsius: "))
+        fahrenheit = celsius_to_fahrenheit(celsius)
+        print("Result: {:.2f} F".format(fahrenheit))
+    elif choice == "F":
+        fahrenheit = float(input("Fahernheit: "))
+        celsius = fahrenheit_to_celsius(fahrenheit)
+        print("Result: {:.2f} C".format(celsius))
+    else:
+        print("Invalid option")
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = celsius * 9 / 5 + 32
+    return fahrenheit
+
+
+main()

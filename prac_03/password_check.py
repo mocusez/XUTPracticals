7510bbd06409a45f9a3584164d975d7cd3795ab3
Mocus EZ <285918468@qq.com>
2021-09-28 21:06:08 +0800

    change prac_03 password_check.py for demand

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,9 +1,21 @@
+LENGTH = 6
+
+
 def main():
-    password = input("Your Password:")
-    check(password)
+    password = get_password(LENGTH)
+    print_asterisks(password)
 
 
-def check(password):
-    print("*"*len(password))
+def get_password(minimum_length):
+    password = input("Enter password of at least {} characters: ".format(minimum_length))
+    while len(password) < minimum_length:
+        print("Password too short")
+        password = input("Enter password of at least {} characters: ".format(minimum_length))
+    return password
+
+
+def print_asterisks(password):
+    print("*" * len(password))
+
 
 main()

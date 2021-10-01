3febe459550d63ad71b581758fe15714d6e2c29e
Mocus EZ <285918468@qq.com>
2021-10-01 22:20:26 +0800

    Update emails.py

---
+++ prac_05/emails.py
@@ -0,0 +1,59 @@
+def main():
+    email_to_name = {}
+    email = input_email()
+    while email != "":
+        name = format_name(email)
+        print("Is your name {}? (Y/n) ".format(name), end="")
+        if not judge_input():
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ").title()
+    for email, name in email_to_name.items():
+        print("{} ({})".format(name, email))
+
+
+#  A silly solution
+# def format_name(email: str):
+#     email = email.title().split("@")[0]
+#     temp = list(email)
+#     result = []
+#     for char in temp:
+#         if char == ".":
+#             char = " "
+#         result.append(char)
+#     return "".join(result)
+
+
+def format_name(email: str):
+    prefix = email.title().split("@")[0]
+    result = prefix.split(".")
+    return "".join(result)
+
+
+def judge_input():
+    result = input().upper()[0]
+    while result != "":
+        if result == "Y":
+            return True
+        elif result == "N":
+            return False
+        else:
+            print("Invalid input!")
+            result = input().upper()[0]
+
+
+def input_email():
+    email = input("Email: ")
+    while email != "":
+        if "@" in email:
+            return email
+        else:
+            print("Invalid Email!")
+            email = input("Email: ")
+    if email == "":
+        print("Invalid Email!")
+        email = input_email()
+    return email
+
+
+main()
